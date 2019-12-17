02b6d652149c0f67046dc60f65b2e0a6f8694934
shwetankv1996 <shwetankv007@gmail.com>
2019-12-17 23:08:54 +0530

    first commit to the project

---
+++ Switch_Tx/transmitter.c
@@ -0,0 +1,49 @@
+#include <8052.h>
+ 
+void delay(void);
+void delay_s(void);
+void UART_Init(void);
+void Transmit_data(char);
+ 
+void main(void)
+{
+UART_Init();
+    while(1)
+    {
+Transmit_data('X');
+         P2 = 0xAA; // Turn ON all LED's connected to Port1
+         delay();
+         P2 = 0x55; // Turn OFF all LED's connected to Port1
+         delay();
+    }
+}
+ 
+void delay_s(void)
+{
+int i;
+for (i = 0; i<0xff; i++)
+	delay();
+}
+
+void delay(void)
+{
+    int i,j;
+    for(i=0;i<0xff;i++)
+         for(j=0;j<0xff;j++);
+}
+
+void UART_Init()
+{
+	TMOD = 0x20;		/* Timer 1, 8-bit auto reload mode */
+	TH1 = 0xFD;		/* Load value for 9600 baud rate */
+	SCON = 0x50;		/* Mode 1, reception enable */
+	TR1 = 1;		/* Start timer 1 */
+}
+
+void Transmit_data(char tx_data)
+{
+	SBUF = tx_data;		/* Load char in SBUF register */
+	while (TI==0);		/* Wait until stop bit transmit */
+	TI = 0;			/* Clear TI flag */
+}
+
